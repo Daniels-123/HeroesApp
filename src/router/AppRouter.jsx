a414e2983afed2259d13b355c4f8e3d480567b45
Daniels-123 <danielbuitragosarmiento@gmail.com>
2023-07-18 21:25:36 -0500

    Add Public Route and Private Route, protection of routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 
 import { HeroesRoutes } from '../heroes';
 import { LoginPage } from '../auth';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 
 
@@ -9,17 +11,28 @@ export const AppRouter = () => {
   return (
     <>
 
-        <Routes>
-            
-            <Route path="login" element={<LoginPage />} />
-            
-            
-            <Route path="/*" element={ <HeroesRoutes />} />
-            
-            
 
+        <Routes>
+
+        <Route path="login/*" element= {
+          <PublicRoute>
+            <Routes>
+            <Route path="/*" element={<LoginPage/>} />
+            </Routes>
+          </PublicRoute>
+        
+        }/>
+
+
+            <Route path="/*" element={
+              <PrivateRoute>
+              <HeroesRoutes/>
+              </PrivateRoute>
+            } />
         </Routes>
     
+            {/* <Route path="/*" element={ <HeroesRoutes />} /> */}
+            {/* <Route path="login" element={<LoginPage />} /> */}
     </>
   )
 }
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,11 +1,18 @@
+import { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth/context/AuthContext';
 
 
 export const Navbar = () => {
 
+ 
+    const {user, logout} = useContext(AuthContext);
+    console.log('user', user);
+
     const navigate = useNavigate();
 
     const onLogout = () => {
+        logout();
         navigate('/login', {
             replace: true
         });
@@ -51,7 +58,7 @@ export const Navbar = () => {
                 <ul className="navbar-nav ml-auto">
                    
                     <span className="nav-item nav-link text-primary">
-                        Daniel
+                        { user?.name}
                     </span>
 
                     <button
